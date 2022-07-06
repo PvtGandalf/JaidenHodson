8df15936342a71447d955f1fa0f61359fb55b1cc
Jaiden <55996217+PvtGandalf@users.noreply.github.com>
2022-07-05 21:45:46 -0700

    Moved suspense call
    - Suspense encapsulates entire canvas

--- src/components/player.js
+++ src/components/player.js
@@ -22,23 +22,23 @@ export default function Player(props) {
 	const modelAngle = Math.PI / 2;
 	
 	return (
-		<Canvas
-			camera={{ position: [2, 0, 12.25], fov: 10 }}
-			width={props.width}
-			height={props.height}
-			style={{
-					backgroundColor: 'transparent',
-					width: '100%',
-					height: '100%',
-			 }}
-		>
-				<ambientLight intensity={1.25} />
-				<ambientLight intensity={0.1} />
-				<directionalLight intensity={0.4} />
-				<Suspense fallback={null}>
+		<Suspense fallback={null}>
+			<Canvas
+				camera={{ position: [2, 0, 12.25], fov: 10 }}
+				width={props.width}
+				height={props.height}
+				style={{
+						backgroundColor: 'transparent',
+						width: '100%',
+						height: '100%',
+				 }}
+			>
+					<ambientLight intensity={1.25} />
+					<ambientLight intensity={0.1} />
+					<directionalLight intensity={0.4} />
 					<Model position={[0, -0.95, 0]} />
-				</Suspense>
-				<OrbitControls enableZoom={false} minPolarAngle={modelAngle} maxPolarAngle={modelAngle}/>
-		</Canvas>
+					<OrbitControls enableZoom={false} minPolarAngle={modelAngle} maxPolarAngle={modelAngle}/>
+			</Canvas>
+		</Suspense>
   );
 }
