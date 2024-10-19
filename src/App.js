b7a08d4c31ea98d9ce2c1276f4796d930697dd2e
Jaiden <55996217+PvtGandalf@users.noreply.github.com>
2024-10-18 17:03:19 -0700

    Added app-ads.txt file
    - Added app-ads.txt file for AdMob support

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Blog from './pages/blog';
 import Posts from './pages/posts';
 import Contact from './pages/contact';
 import PrivacyPolicy from './pages/privacyPolicy';
+import AppAdsFile from './pages/app-ads';
 import Error from './pages/error';
 
 const App = () => useRoutes([
@@ -19,6 +20,7 @@ const App = () => useRoutes([
     { path: "/blog/posts/:path", element: <Posts /> },
     { path: "/contact", element: <Contact /> },
     { path: "/privacyPolicy", element: <PrivacyPolicy /> },
+    { path: "/app-ads.txt", element: <AppAdsFile /> },
     { path: "*", element: <Error /> }
   ]);
 
