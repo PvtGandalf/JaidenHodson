e49922525c23036db8936f15f2761428703e377a
Jaiden <55996217+PvtGandalf@users.noreply.github.com>
2024-10-21 09:54:50 -0700

    Moved app-ads.txt to public folder
    - Web scrapers can now access app-ads.txt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Blog from './pages/blog';
 import Posts from './pages/posts';
 import Contact from './pages/contact';
 import PrivacyPolicy from './pages/privacyPolicy';
-import AppAdsFile from './pages/app-ads';
 import Error from './pages/error';
 
 const App = () => useRoutes([
@@ -20,7 +19,6 @@ const App = () => useRoutes([
     { path: "/blog/posts/:path", element: <Posts /> },
     { path: "/contact", element: <Contact /> },
     { path: "/privacyPolicy", element: <PrivacyPolicy /> },
-    { path: "/app-ads.txt", element: <AppAdsFile /> },
     { path: "*", element: <Error /> }
   ]);
 
